2e845a33fe89c5c4d760fa3a34eae00582636149
ryon labaw <rlabaw@gmail.com>
2023-05-15 10:51:18 -0700

    initial

--- examples/lights/lights.py
+++ examples/lights/lights.py
@@ -11,8 +11,7 @@ import argparse
 
 # LED strip configuration:
 LED_COUNT = 8        # Number of LED pixels.
-LED_PIN = 13          # GPIO pin connected to the pixels (18 uses PWM!).
-# LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_PIN = 13          # GPIO pin connected to the pixels (13 uses PWM!).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 50  # Set to 0 for darkest and 255 for brightest
